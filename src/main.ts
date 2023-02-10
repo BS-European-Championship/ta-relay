202d5f3c2f6d427e690ee1b33b2a09b357073f72
Flick (Alex) <flameflick@pm.me>
2023-02-11 00:32:50 +0600

    feat(main): use env config

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import type { Interaction, Message } from "discord.js";
 import { IntentsBitField } from "discord.js";
 import { Client } from "discordx";
 import { TARelay } from "./ta-relay/relay";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const bot = new Client({
   // To use only guild command
@@ -52,20 +55,9 @@ bot.on("messageCreate", (message: Message) => {
 });
 
 async function run() {
-  // The following syntax should be used in the commonjs environment
-  //
-  // await importx(__dirname + "/{events,commands}/**/*.{ts,js}");
-
-  // The following syntax should be used in the ECMAScript environment
   await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
 
-  // Let's start the bot
-  if (!process.env.BOT_TOKEN) {
-    throw Error("Could not find BOT_TOKEN in your environment");
-  }
-
-  // Log in with your bot token
-  await bot.login(process.env.BOT_TOKEN);
+  await bot.login(process.env.BOT_TOKEN!);
 }
 
 export const relay = new TARelay();
